276c869214aff1c6e00050c7e6313830965144c0
Bloquecitos <francisco.vidal2@upr.edu>
2025-09-05 15:11:26 -0400

    Removing TODO from task 1

--- Game.py
+++ Game.py
@@ -79,7 +79,7 @@ bg_color = pygame.Color('grey12')
 
 # Game Rectangles (ball and player paddle)
 ball = pygame.Rect(screen_width / 2 - 15, screen_height / 2 - 15, 30, 30)  # Ball (centered)
-# TODO Task 1 Make the paddle bigger
+# Task 1 Make the paddle bigger
 player_height = 15
 player_width = 200
 player = pygame.Rect(screen_width/2 - 45, screen_height - 20, player_width, player_height)  # Player paddle
